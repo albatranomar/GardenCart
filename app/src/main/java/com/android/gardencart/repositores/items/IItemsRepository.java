dcbd9bc004e326f22be1e685f8a97034e01bda57
albatranomar <albatranomar943@gmail.com>
2025-04-17 18:38:14 +0300

    refactor: new utility methods

--- app/src/main/java/com/android/gardencart/repositores/items/IItemsRepository.java
+++ app/src/main/java/com/android/gardencart/repositores/items/IItemsRepository.java
@@ -6,5 +6,8 @@ import java.util.List;
 
 public interface IItemsRepository {
     List<Item> getItems();
+    Item getItemByItemId(String itemId);
     List<String> getTags();
+    void resetItems();
+    void putItems(List<Item> newItems);
 }
