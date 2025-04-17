060f2b9c00a9181533e0c82be155a39d75df941c
albatranomar <albatranomar943@gmail.com>
2025-04-17 18:37:50 +0300

    refactor: abstract Preferences keys to a single location.

---
+++ app/src/main/java/com/android/gardencart/Constants.java
@@ -0,0 +1,10 @@
+package com.android.gardencart;
+
+public class Constants {
+    public static String USER_USERNAME = "USER_USERNAME";
+    public static String KEEP_LOGIN = "KEEP_LOGIN";
+
+    public static String USERS_DATA = "USERS_DATA";
+
+    public static String ITEMS_DATA = "ITEMS_DATA";
+}
